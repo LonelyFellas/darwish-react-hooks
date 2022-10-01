a26755b8aea712ca3b4c45513f87f87d555fcede
darwish <289582304@qq.com>
2022-10-02 06:49:50 +0800

    cloneDeep.ts function done!

---
+++ src/functions/cloneDeep.ts
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param data 实现一个深拷贝。
+ */
+const cloneDeep: <T>(data: T) => T | { [k: string]: any; } = (data) => {
+    if (Array.isArray(data)) {
+        return data.map((child) => cloneDeep(child))
+    }
+    if (typeof data === 'object' && data !== null) {
+        return Object.fromEntries(Object.entries(data).map(([k, v]) => [k, cloneDeep(v)]))
+    }
+    return data
+}
+
+export default cloneDeep;
--- src/functions/isBlanks.ts
+++ src/functions/isBlanks.ts
@@ -1,6 +1,6 @@
 /**
  *
- * @param data 判断真假判断所需的数据
+ * @param data 支持所有基本类型,还有[],{},Set,Map,Date的判断，true则数据空或者数据错误
  */
 const isBlanks: <T>(data:T) => boolean = (data) => {
     if(data == Infinity) {

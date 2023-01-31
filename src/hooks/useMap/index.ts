eee4e4906b4f5bad00978a1000175a29923fc048
LonelyFellas <289582304@qq.com>
2023-01-31 22:03:37 +0800

    add useMap hook

---
+++ src/hooks/useMap/index.ts
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+interface IUseMapParams {
+  [P: string | number]: unknown;
+}
+
+const useMap = <T extends object = any>(defaultMapValue: T = {} as T) => {
+  const [mapState, setMapState] = React.useState(defaultMapValue);
+
+  const actions = React.useMemo(() => {
+    const set = <K extends keyof IUseMapParams>(
+      keyProp: K,
+      valueProp: unknown
+    ) => {
+      setMapState((prev) => ({ ...prev, [keyProp]: valueProp }));
+    };
+
+    const setAll = <U extends T>(allValue: U) => {
+      setMapState((prev) => ({ ...prev, ...allValue }));
+    };
+
+    const remove = <K extends keyof T>(removeValue: K) => {
+      setMapState((prev) => {
+        const  { [removeValue]: omit, ...rest} = prev;
+        return rest as T;
+      });
+    };
+
+    const reset = () => {
+      setMapState(mapState);
+    };
+
+    return {
+      get: React.useCallback((key: keyof T) => mapState[key], []),
+      set,
+      setAll,
+      remove,
+      reset,
+    };
+  }, []);
+
+  return [mapState, actions] as const;
+};
+
+export default useMap;
